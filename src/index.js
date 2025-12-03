c4a603f933826d6ce95dcf477a0fc85451609cb5
Riley Drellishak <rileydrellishak@gmail.com>
2025-12-03 14:45:25 -0500

    mid colorTempValue function

---
+++ src/index.js
@@ -0,0 +1,61 @@
+// display a default value for temp
+// when clicking up arrow, temp += 1
+// when clicking down arrow, temp -= 1
+
+const state = {
+  tempNum: 72,
+  tempValue: null,
+  tempColor: null
+};
+
+const colorTempValue = (tempNum) => {
+  let tempValue = document.querySelector('#tempValue');
+  let currentColor = tempValue.classList;
+  if (tempNum >= 80) {
+    tempValue.classList.replace(currentColor, 'red');
+  } else if (tempNum <= 49) {
+    tempValue.classList.replace(currentColor, 'teal');
+  }
+}
+
+const increaseTemp = (event) => {
+  let tempValue = document.querySelector('#tempValue');
+  state.tempNum += 1;
+  tempValue.textContent = `${state.tempNum}`;
+  colorTempValue(state.tempNum);
+};
+
+const decreaseTemp = (event) => {
+  let tempValue = document.querySelector('#tempValue');
+  state.tempNum -= 1;
+  tempValue.textContent = `${state.tempNum}`;
+};
+
+// change color of temp text according to table in readme
+
+// temperature ranges change landscape in weather garden according to table in readme
+
+// register event handlers
+
+const registerEventHandlers = () => {
+  const increaseTempControl = document.querySelector('#increaseTempControl');
+  increaseTempControl.addEventListener('click', increaseTemp);
+  const decreaseTempControl = document.querySelector('#decreaseTempControl');
+  decreaseTempControl.addEventListener('click', decreaseTemp);
+  // const changeTempColor = document.querySelector('#tempValue');
+  // changeTempColor.addEventListener('', colorTempValue);
+};
+
+
+const loadControls = () => {
+  state.tempValue = document.getElementById('tempValue');
+  state.tempValue.textContent = state.tempNum;
+  state.tempValue.classList.add('orange');
+};
+
+const onLoaded = () => {
+  loadControls();
+  registerEventHandlers();
+};
+
+onLoaded();
